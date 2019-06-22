9a556f7d601e68a5b12d305506c24252327c0bc7
Gabriel Fuhr <gt.fuhr@gmail.com>
2019-06-22 00:01:42 -0300

    Update

--- benchmarks.py
+++ benchmarks.py
@@ -1,45 +1,189 @@
-import os
+import subprocess as sp
+import re
+
 # RAM INFO to be used
 # Cycle time (ns):  13.6692
 # Read Energy (nJ): 8.64848
 def sim_cache(associatividade, tamanho_do_bloco, numero_de_blocos):
-    #./sim-cache -cache:il1 il1:512:32:1:l -cache:dl1 dl1:256:16:4:l  -cache:dl2 none -cache:il2 none ~/benchmarks/mm.ss
     cache_instrucoes = "-cache:il1 il1:" + str(numero_de_blocos[0]) + ":" + str(tamanho_do_bloco[0]) + ":" + str(associatividade[0]) + ":l"
     cache_dados = "-cache:dl1 dl1:" + str(numero_de_blocos[1]) + ":" + str(tamanho_do_bloco[1]) + ":" + str(associatividade[1]) + ":l"
     benchmark_path = "./benchmarks/benchs_trabalho/"
     comando = "./simplesim-3.0/sim-cache " + cache_instrucoes + " " + cache_dados + " -cache:dl2 none -cache:il2 none " + benchmark_path
-    benchmarks = ["amp.ss","mm.ss","basicmath.ss"]
-    benchmark_results = [os.popen(comando + benchmark) for benchmark in benchmarks]
-    benchmark_treated_results = [trata_retorno_sim_cache(benchmark) for benchmark in benchmarks]
+    # benchmarks = ["amp.ss","mm.ss","basicmath.ss"]
+    benchmarks = ["mm.ss"]
+    benchmark_results = [sp.getoutput(comando + benchmark) for benchmark in benchmarks]
+    benchmark_treated_results = [trata_retorno_sim_cache(benchmark) for benchmark in benchmark_results]
     return benchmark_treated_results
 
+
+def generate_benchmark_data(benchmark_treated_list):
+    sim_cache_data = {}
+
+    for line in benchmark_treated_list:
+        infos = re.split(' +',line) # Split the line using the space as separator two times
+        key, value = infos[0], infos[1]
+        sim_cache_data[key] = value        
+    
+    return sim_cache_data
+
+
+
 def trata_retorno_sim_cache(benchmark):
-    return benchmark
+    # Exemplo das linhas desejadas
+    
+    # il1.hits                             7701959 # total number of hits		NCH 
+    # il1.misses                       531 # total number of misses		NCM
+    # il1.writebacks                    0 # total number of writebacks		NWB
+    
 
-def cacti(associatividade, tamanho_do_bloco, numero_de_blocos):
-    pass
+    # dl1.hits                            1043160 # total number of hits		NCH
+    # dl1.misses                      1387 # total number of misses		NCM
+    # dl1.writebacks                  355 # total number of writebacks		NWB
 
-def trata_retorno_cacti():
-    pass
+    benchmark = benchmark.replace("\n",'\n')
+    benchmark_list = benchmark.splitlines()
+    benchmark_list_treated = []
+
+    
+    for line in benchmark_list:
+        if line.count("il1.hits"):
+            benchmark_list_treated.append(line)
+        elif line.count("il1.misses"):
+            benchmark_list_treated.append(line)
+        elif line.count("il1.writebacks"):
+            benchmark_list_treated.append(line)
+
+
+        elif line.count("dl1.hits"):
+            benchmark_list_treated.append(line)
+        elif line.count("dl1.misses"):
+            benchmark_list_treated.append(line)
+        elif line.count("dl1.writebacks"):
+            benchmark_list_treated.append(line)
+
+    sim_cache_data = generate_benchmark_data(benchmark_list_treated)
+    return sim_cache_data
+
+
+def generate_cacti_data(benchmark_treated_list):
+    sim_cache_data = {}
+
+    for line in benchmark_treated_list:
+        infos = re.split(' +',line) # Split the line using the space as separator two times
+        if(line.count("Cache height x width (mm):") == 0):
+            key, value = infos[1], infos[4]
+        else:
+            key, value = infos[1], (infos[8], infos[6])
+        sim_cache_data[key] = value        
+    
+    return sim_cache_data
+
+def a(x):
+    print(x)
+    exit(0)
+
+def trata_retorno_cacti(cacti):
+    # Exemplo das linhas desejadas
+
+    # Access time (ns): 1.24238		LAC
+    # Read Energy (nJ): 0.0361295		EAC
+    # Cache height x width (mm): 0.23248 x 0.122094
+    cacti = cacti.replace("\n",'\n')
+    cacti_list = cacti.splitlines()
+    cacti_list_treated = []
+
+    
+    for line in cacti_list:
+        if line.count("Access time (ns):"):
+            cacti_list_treated.append(line)
+        elif line.count("Read Energy (nJ):"):
+            cacti_list_treated.append(line)
+        elif line.count("Cache height x width (mm):"):
+            cacti_list_treated.append(line)
+
+    cacti_data = generate_cacti_data(cacti_list_treated)
+    return cacti_data
+
+def cacti(associatividade, tamanho_do_bloco, numero_de_blocos, cache_num):
+    comando = "./cacti65/cacti -infile cache.cfg"
+    cacti_all_results = []
+    for i in range(cache_num):
+        tamanho_total = tamanho_do_bloco[i] * numero_de_blocos[i]
+
+        cache_cfg = """
+        # Cache size
+        -size (bytes) {tamanho_tot}
+
+        # Line size
+        -block size (bytes) {bloco_tamanho}
+
+        # To model Fully Associative cache, set associativity to zero
+
+        -associativity {associativi}
+        """.format(tamanho_tot = tamanho_total, bloco_tamanho = tamanho_do_bloco[i], associativi = associatividade[i])
+
+        cache_cfg_rest = open("./cacti65/cache_rest.cfg","r").readlines()
+        cache_cfg = cache_cfg + str(cache_cfg_rest).replace("\n",'\n')
+        open("./cacti65/cache.cfg",'w').write(cache_cfg)
+
+        cacti_results = sp.getoutput(comando)
+        cacti_all_results.append(cacti_results)
+    
+    cacti_treated_results = [trata_retorno_cacti(cacti) for cacti in cacti_all_results]
+    return cacti_treated_results
+
 
 def salva_resultados():
     pass
 
-
-def main():
-
-    # Inicia um banco de dados com os resultados
-    
-    associatividade= [1,1]
-    tamanho_do_bloco = [8,8]
-    numero_de_blocos = [32,32]
+def testa_cache(associatividade,tamanho_do_bloco,numero_de_blocos):
+    """
+        Recebe três listas contendo a associatividade, tamanho dos blocos e os numeros de blocos da 
+        cache de instruções e da cache de dados 
+    """
     # Roda o sim-cache nos três diferentes programas de benchmark
     sim_cache_results = sim_cache(associatividade, tamanho_do_bloco, numero_de_blocos)
-    print(sim_cache_results) 
+    
     # Roda o cacti com a saída do sim-cache 
-    # cact_results = cacti(associatividade, tamanho_do_bloco, numero_de_blocos)
-    # cact_results = trata_retorno_cacti(cact_results)
-    #     # Trata o retorno do cacti
+    cact_results = cacti(associatividade, tamanho_do_bloco, numero_de_blocos,len(associatividade))
+    
+    return sim_cache_results, cact_results
+
+
+def main():
+    # Inicia um banco de dados com os resultados
+    # To be done
+    
+    associatividade = [
+        1, # Associatividade memória cache de Instruções
+        1  # Associatividade memória cache de Dados
+    ]
+    tamanho_do_bloco = [
+        8, # Tamanho do bloco da memória cache Instruções
+        8  # Tamanho do bloco da memória cache Dados
+    ]
+    numero_de_blocos = [
+        32, # Numero de blocos da memória cache Instruções
+        32  # Numero de blocos da memória cache Dados
+    ]
+    sim_cache_data, cacti_data = testa_cache(associatividade,tamanho_do_bloco,numero_de_blocos)
+
+    NCH = 
+    # Tempo total da memoria de instruções:
+    # tempo_total_INST = NCH*LAC + NCM*LAC +NCM*LAMP + NWB*LAMP 
+
+    # Tempo total da memoria de dados:
+    # tempo_total_DADOS = NCH*LAC + NCM*LAC +NCM*LAMP + NWB*LAMP 
+    # (onde LAMP é Latência de um Acesso à Mem. Principal, NWB é o Número de writebacks)
+
+
+    # Energia total da memoria de instruções:
+    # energia_total_ = NCH*EAC + NCM*EAC +NCM*EAMP + NWB*EAMP 
+
+    # Energia total da memoria de dados:
+    # energia_total_DADOS = NCH*EAC + NCM*EAC +NCM*EAMP + NWB*EAMP 
+    # (onde EAMP é Energia de um Acesso à Mem. Principal)
+
     print("FIM")
 
 if __name__ == "__main__":
